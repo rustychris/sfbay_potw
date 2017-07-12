9128a8b03cc04315bc39f3b3dd65af63e4e05ff2
Rusty Holleman <rustyh@sfei.org>
2017-07-12 16:30:13 -0700

    Make wwtp_sources.py more consistent
    Naming schemes, selective use of multiple datasets.

--- scripts/wwtp_sources.py
+++ scripts/wwtp_sources.py
@@ -1,8 +1,20 @@
+"""
+Clean source data from wastewater treatment plants, writing 
+intermediate csv files with uniform naming conventions, units,
+date formats, etc.
+
+Limited to Delta plants.
+"""
+import os
 import numpy as np
 import pandas as pd
 import datetime as dt
 import matplotlib.pyplot as plt
 import xarray as xr
+
+from stompy import utils # for date utilities
+
+### Utility functions
 
 def load_wwtp(filename, tname, tformat, varname, unitsname, units, method, cols, vname):
     dat = pd.read_csv(filename, usecols=cols)
@@ -11,6 +23,7 @@ def load_wwtp(filename, tname, tformat, varname, unitsname, units, method, cols,
             if (dat[unitsname][i] == units and pd.isnull(dat[method][i]))]
     var = [dat[varname][i] for i in range(len(dat[varname])) \
            if (dat[unitsname][i] == units and pd.isnull(dat[method][i]))]
+
     var = np.asarray(var)
     d = {'Time': pd.Series(time),
          vname: pd.Series(var)}
@@ -38,8 +51,17 @@ def day_avg(time, var, ind):
         d += len(np.where(ind==i)[0])
     return date, dvar
     
-    
-# variables that don't change between wwtp files
+def var_fields(date, variable):
+    time = []
+    var = []
+    for i in range(len(date)):
+        if np.isnan(float(variable[i]))==False:
+            time.append(dt.datetime.strptime(date[i], "%m/%d/%Y"))
+            var.append(variable[i]) 
+    return time, np.asarray(var)
+
+### variables that don't change between wwtp files
+
 inpath = "../sources/delta_sources/"    
 outpath = "../outputs/intermediate/delta/"
 tname = "Sampling Date Time"
@@ -51,6 +73,7 @@ cols = [3,5,6,12]
 prat = 2.23
 
 ### DAVIS
+
 # ammonia
 filename = inpath + "Davis_5A570100001_Ammonia_Total_as_N.csv"
 outfile = outpath + "Davis_Ammonia.csv"
@@ -319,7 +342,8 @@ if 0: # Manteca1.csv is no longer generated.  Using Manteca.csv at bottom of thi
     manteca.to_csv(outpath+"manteca1.csv")
 
 ### TRACY
-if 0:
+
+if 1: # Tracy, part A
     # ammonia
     filename = inpath + "Tracy_5B390108001_Ammonia_Total_as_N.csv"
     outfile = outpath + "Tracy_Ammonia.csv"
@@ -329,7 +353,7 @@ if 0:
     ind = day_ind(am_df.Time)
     time, am = day_avg(am_df.Time, am_df[vname], ind)
     d = {'Date': pd.Series(time),
-             vname: pd.Series(am)}
+         vname: pd.Series(am)}
     df1 = pd.DataFrame(d)
     #df1.to_csv(outfile)
     # plotting
@@ -338,7 +362,6 @@ if 0:
     ax.set_title("Tracy: Total Ammonia (as N)")
     ax.set_ylabel("mg/L")
     fig.savefig(outpath + "figures/Tracy_Ammonia.png")    
-    
     
     # flow
     filename = inpath + "Tracy_5B390108001_Flow.csv"
@@ -432,13 +455,13 @@ if 0:
     filename = inpath + "Tracy_5B390108001_Phosphorus_Total_as_P.csv"
     outfile = outpath + "Tracy_Phosphorus.csv"
     units = "mg/L"
-    vname = "P mg/L P"
+    vname = "TP mg/L P" # had been just P, but seems more consistent to call this TP
     p_df = load_wwtp(filename, tname, tformat, varname, unitsname, units, method, cols, vname)
     ind = day_ind(p_df.Time)
     time, p = day_avg(p_df.Time, p_df[vname], ind)
     d = {'Date': pd.Series(time),
-             vname: pd.Series(p),
-             'PO4 mg/L P': pd.Series(p/prat)}
+         vname: pd.Series(p),
+         'PO4 mg/L P': pd.Series(p/prat)}
     df6 = pd.DataFrame(d)
     #df6.to_csv(outfile)
     # plotting
@@ -448,20 +471,204 @@ if 0:
     ax.set_ylabel("mg/L")
     fig.savefig(outpath + "figures/Tracy_Phosphorus.png")    
     
-    # concatenate
-    tracy = pd.merge(tracy, df6, how='outer', on='Date')
+    # concatenate - this is just part of the Tracy data, to be merged
+    # with a second tracy dataset below
+    tracyA = pd.merge(tracy, df6, how='outer', on='Date')
+
+    # TRACY, part B
+    filename = inpath + "tracy.csv"
+    tracyB = pd.read_csv(filename,parse_dates=['Date'])
+    tracyB.rename(columns={'Flow': 'flow mgd', 'NH3': 'NH3 mg/L N', 
+                           'NO3': 'NO3 mg/L N', 'TP': 'TP mg/L P'}, inplace=True)
     
-    # save final concatenated file
-    tracy.to_csv(outpath+"tracy.csv")
+    # TRACY, merge A and B
+    # at this point, tracyA has dates as python datetime, tracyB has them as numpy
+    # datetime.
     
-def var_fields(date, variable):
-    time = []
-    var = []
-    for i in range(len(date)):
-        if np.isnan(float(variable[i]))==False:
-            time.append(dt.datetime.strptime(date[i], "%m/%d/%Y"))
-            var.append(variable[i]) 
-    return time, np.asarray(var)
+    
+    outfile = outpath + "tracy.csv"
+    
+##
+
+# make a copy of tracyA to allow safely changing the datatype of Date.
+tracyA2=tracyA.copy()
+tracyA2['Date']=utils.to_dt64(tracyA2['Date'].values)
+
+tracy=pd.merge(tracyA2,tracyB,how='outer',on='Date')
+tracy.sort_values('Date',inplace=True) # merge does not guarantee sorted output
+
+# tracyA2 comes in with         Date  NH3 mg/L N  flow mgd  NO3 mg/L N  NO2 mg/L N  N+N mg/L N 
+#                               P mg/L P  PO4 mg/L P
+# It is from CWIQS data
+
+# tracyB comes in with          Date  flow mgd  NH3 mg/L N  NO3 mg/L N  TP mg/L P
+# This is from 
+# Go analyte by analyte to see what's going on:
+
+## 
+y_missing=tracy['NH3 mg/L N_y'].isnull()
+tracy['NH3 mg/L N']=np.where( ~y_missing,
+                              tracy['NH3 mg/L N_y'],
+                              tracy['NH3 mg/L N_x'])
+
+if 1: # compare NH3
+    fig=plt.figure(1)
+    fig.clf()
+    ax=fig.add_subplot(1,1,1)
+    ax.plot(tracy.Date,tracy['NH3 mg/L N_x'],marker='x',label='NH3 CWIQS')
+    ax.plot(tracy.Date,tracy['NH3 mg/L N_y'],marker='x',label='NH3 Novick')
+    ax.plot(tracy.Date,tracy['NH3 mg/L N'],marker='o',mfc='none',label='NH3 out',lw=0.9,c='k')
+    ax.legend()
+    # y is longer, and has better coverage, with just a few more recent points
+    # coming from only x
+    # overall concentrations quite similar.
+    fig.savefig(outpath+"figures/Tracy-nh3-compare_sources.png")
+
+##
+
+y_missing=tracy['NO3 mg/L N_y'].isnull()
+tracy['NO3 mg/L N']=np.where( ~y_missing,
+                              tracy['NO3 mg/L N_y'],
+                              tracy['NO3 mg/L N_x'])
+
+if 1: # compare NO3
+    # on the occasions that there is data from both (mid-2010 to early 2013)
+    # 'x' (A2) is about 1/3 the value of 'y' (B).  The higher values line up
+    # with previous levels and later levels, and even later levels from A2,
+    # suggesting that we should go with the B values when both are present.
+    fig=plt.figure(1)
+    fig.clf()
+    ax=fig.add_subplot(1,1,1)
+    ax.plot(tracy.Date,tracy['NO3 mg/L N_x'],marker='x',label='NO3 CWIQS')
+    ax.plot(tracy.Date,tracy['NO3 mg/L N_y'],marker='x',label='NO3 Novick')
+    ax.plot(tracy.Date,tracy['NO3 mg/L N'],marker='o',mfc='none',label='NO3 out',lw=0.9,c='k')
+    ax.legend()
+    fig.savefig(os.path.join(outpath,"figures/Tracy-NO3-compare_sources.png"))
+
+##
+
+# NO2 only from tracyA2
+
+##
+
+# NN only from tracyA2, only 2013--on
+# NO2, where present, is much less than 10% of NO3.
+# Generally allow NN to be interchangeable with NO3, adding NO2
+# where data present.
+
+# Specifically:
+# - where NN is missing but we have NO3, set NN to NO3+NO2
+NO2_or_0=tracy['NO2 mg/L N'].copy()
+NO2_or_0.loc[ NO2_or_0.isnull() ]=0.0
+
+NNmissing=tracy['N+N mg/L N'].isnull()
+tracy.loc[NNmissing,'N+N mg/L N'] = tracy.loc[NNmissing,'NO3 mg/L N'] + NO2_or_0
+
+# - where NO3 is missing, fill with NN, less NO2 if available, but non-negative
+NO3missing=tracy['NO3 mg/L N'].isnull()
+tracy.loc[NO3missing,'NO3 mg/L N'] = np.clip(tracy.loc[NO3missing,'N+N mg/L N'] - NO2_or_0,0,np.inf)
+
+# and a few NN samples which are oddly low, and way below a concurrent NO3 sample
+tracy['N+N mg/L N'] = np.maximum(tracy['N+N mg/L N'], tracy['NO3 mg/L N'])
+
+if 1: # compare NN, NO3, NO2 
+    fig=plt.figure(1)
+    fig.clf()
+    ax=fig.add_subplot(1,1,1)
+    ax.plot(tracy.Date,tracy['N+N mg/L N'],marker='x',label='NN: Tracy_5B390108001_Nitrite+Nitrate_as_N.csv')
+    ax.plot(tracy.Date,tracy['NO2 mg/L N'],marker='x',label='NO2: Tracy_5B390108001_Nitrite_Total_as_N.csv')
+    ax.plot(tracy.Date,tracy['NO3 mg/L N'],marker='o',mfc='none',label='NO3: Tracy_5B390108001_Nitrate_Total_as_N.csv')
+    ax.legend()
+    fig.savefig(os.path.join(outpath,"figures/Tracy-NOx-compare_sources.png"))
+
+##
+
+# Phosphorus:
+if 1:
+    # compare P, PO4, TP.
+    # Of course TP is the highest, but it is much higher than either of P, PO4
+    # There are also numerous samples with coincident P and PO4, with P being maybe
+    # twice PO4.
+    # These appear to be measuring truly different things, and there is nothing
+    # to be gained by any combinations.  Leave as is.
+
+    # Hmm - tracyA2['P mg/L P'] is from "Tracy_5B390108001_Phosphorus_Total_as_P.csv"
+    # so better named as 'TP mg/L P', which will then be TP mg/L P_x
+    # And PO4 stays as a constant ratio times that CWIQS value
+    fig=plt.figure(1)
+    fig.clf()
+    ax=fig.add_subplot(1,1,1)
+    ax.plot(tracy.Date,tracy['TP mg/L P_x'],marker='x',  label='Tracy_5B390108001_Phosphorus_Total_as_P.csv')
+    ax.plot(tracy.Date,tracy['TP mg/L P_y'],marker='x', label='TP:tracy.csv')
+    ax.plot(tracy.Date,tracy['PO4 mg/L P'],marker='x',label='PO4~%.2f*Tracy_5B390108001_Phosphorus_Total_as_P.csv'%prat)
+    ax.legend()
+    fig.savefig(os.path.join(outpath,"figures/Tracy-P-compare_sources.png"))
+
+##
+
+# Flow:
+tracy['flow mgd']=tracy['flow mgd_x']
+
+if 1:
+    # Hmm - these appear to be very different datasets, too.
+    # Emma suggests that the metadata for CWIQS is more reliable than
+    # the older dataset, so we should use flow_mgd_x, and discard flow_mgd_y
+    # permitted flow is capped at 16 mgd, so no help there in understanding
+    # which is correct.
+    fig=plt.figure(1)
+    fig.clf()
+    ax=fig.add_subplot(1,1,1)
+    # This is the CWIQS data
+    ax.plot(tracy.Date,tracy['flow mgd_x'],marker='x',label='delta_sources/Tracy_5B390108001_Flow.csv')
+    # This is from Emily's older data stash
+    ax.plot(tracy.Date,tracy['flow mgd_y'],marker='x',label='delta_sources/tracy.csv')
+    ax.plot(tracy.Date,tracy['flow mgd'],marker='o',lw=0.8,color='k',mfc='none',label='output')
+    ax.set_ylabel('flow mgd')
+    ax.grid(1)
+    ax.legend()
+    fig.savefig(os.path.join(outpath,"figures/Tracy-flow-compare_sources.png"))
+
+# output the final columns:
+tracy_final=tracy.loc[:, ['Date','NH3 mg/L N_x', 'flow mgd_x', 'NO3 mg/L N_x', 'NO2 mg/L N',
+                          'N+N mg/L N', 'P mg/L P', 'PO4 mg/L P', 'flow mgd_y', 'NH3 mg/L N_y',
+                          'NO3 mg/L N_y', 'TP mg/L P', 'NH3 mg/L N', 'NO3 mg/L N', 'flow mgd'] ]
+tracy_final.to_csv(outfile)
+    
+##
+df=tracy
+# plotting
+date, var = var_fields(df["Date"], df["flow mgd"])
+fig, ax = plt.subplots(figsize=(8,4))
+ax.plot(date, var, '-x', color="cadetblue")
+ax.set_title("Tracy: Flow")
+ax.set_ylabel("MGD")
+fig.savefig(outpath + "figures/Tracy_Flow.png")
+
+date, var = var_fields(df["Date"], df["NH3 mg/L N"])
+fig, ax = plt.subplots(figsize=(8,4))
+ax.plot(date, var, '-x', color="darkslategray")
+ax.set_title("Tracy: Ammonia")
+ax.set_ylabel("mg/L")
+fig.savefig(outpath + "figures/Tracy_Ammonia.png")
+
+date, var = var_fields(df["Date"], df["NO3 mg/L N"])
+fig, ax = plt.subplots(figsize=(8,4))
+ax.plot(date, var, '-x', color="seagreen")
+ax.set_title("Tracy: Nitrate")
+ax.set_ylabel("mg/L")
+fig.savefig(outpath + "figures/Tracy_Nitrate.png")
+
+date, var = var_fields(df["Date"], df["TP mg/L P"])
+fig, ax = plt.subplots(figsize=(8,4))
+ax.plot(date, var, '-x', color="indianred")
+ax.set_title("Tracy: Total Phosphorus")
+ax.set_ylabel("mg/L")
+fig.savefig(outpath + "figures/Tracy_Phosphorus.png")
+
+
+
+    
+    
 
 ### REGIONAL SAN
 filename = inpath + "SacRegional.csv"
@@ -556,41 +763,6 @@ fig.savefig(outpath + "figures/Stockton_Nitrite.png")
 #ax.set_ylabel("mg/L")
 #fig.savefig(outpath + "figures/Stockton_Phosphorus.png")
 
-### TRACY
-filename = inpath + "tracy.csv"
-outfile = outpath + "tracy.csv"
-df = pd.read_csv(filename)
-df.rename(columns={'Flow': 'flow mgd', 'NH3': 'NH3 mg/L N', 
-                   'NO3': 'NO3 mg/L N', 'TP': 'TP mg/L P'}, inplace=True)
-df.to_csv(outfile)
-# plotting
-date, var = var_fields(df["Date"], df["flow mgd"])
-fig, ax = plt.subplots(figsize=(8,4))
-ax.plot(date, var, '-x', color="cadetblue")
-ax.set_title("Tracy: Flow")
-ax.set_ylabel("MGD")
-fig.savefig(outpath + "figures/Tracy_Flow.png")
-
-date, var = var_fields(df["Date"], df["NH3 mg/L N"])
-fig, ax = plt.subplots(figsize=(8,4))
-ax.plot(date, var, '-x', color="darkslategray")
-ax.set_title("Tracy: Ammonia")
-ax.set_ylabel("mg/L")
-fig.savefig(outpath + "figures/Tracy_Ammonia.png")
-
-date, var = var_fields(df["Date"], df["NO3 mg/L N"])
-fig, ax = plt.subplots(figsize=(8,4))
-ax.plot(date, var, '-x', color="seagreen")
-ax.set_title("Tracy: Nitrate")
-ax.set_ylabel("mg/L")
-fig.savefig(outpath + "figures/Tracy_Nitrate.png")
-
-date, var = var_fields(df["Date"], df["TP mg/L P"])
-fig, ax = plt.subplots(figsize=(8,4))
-ax.plot(date, var, '-x', color="indianred")
-ax.set_title("Tracy: Total Phosphorus")
-ax.set_ylabel("mg/L")
-fig.savefig(outpath + "figures/Tracy_Phosphorus.png")
 
 ### MANTECA NEW FILE -- this is the one actually used for now.
 filename = inpath + "Manteca_2000_2017.csv"
